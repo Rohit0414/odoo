01c6b42a398ab7691e1dcd5159cf514878ea0720
Rohit <dhimanrohit070@gmail.com>
2025-01-27 17:20:02 +0530

    To_do_list

--- myapp/models.py
+++ myapp/models.py
@@ -11,8 +11,9 @@ class Product(models.Model):
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    completed = models.BooleanField(False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True) 
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True,blank=True)
 
 
     def _str_(self):
