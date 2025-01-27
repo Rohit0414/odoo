ec6fb81e82ea85522ac0cf47762e80039d491a3e
Rohit <dhimanrohit070@gmail.com>
2025-01-27 17:26:21 +0530

    To_do_list

--- myapp/models.py
+++ myapp/models.py
@@ -13,7 +13,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True) 
     completed = models.BooleanField(default=False)
-    due_date = models.DateField(null=True,blank=True)
+    due_date = models.DateField(null=True, blank=True)
 
 
     def _str_(self):
